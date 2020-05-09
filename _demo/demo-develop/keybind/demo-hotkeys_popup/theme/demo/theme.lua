006685d5c5640684ae049334a3e41551b8d4f1e1
samwhelp <samwhelp@users.noreply.github.com>
2020-05-09 19:34:34 +0800

    fix

--- _demo/demo-develop/keybind/demo-hotkeys_popup/theme/demo/theme.lua
+++ _demo/demo-develop/keybind/demo-hotkeys_popup/theme/demo/theme.lua
@@ -5,6 +5,11 @@ local theme = {}
 theme.bg_normal = "#222222"
 theme.fg_normal = "#aaaaaa"
 
+theme.border_width = 2
+theme.border_color_normal = "#000000"
+theme.border_color_active = "#535d6c"
+theme.border_color_marked = "#91231c"
+
 -- https://awesomewm.org/doc/api/libraries/awful.hotkeys_popup.widget.html#beautiful.hotkeys_border_width
 theme.hotkeys_border_width = 2
 
