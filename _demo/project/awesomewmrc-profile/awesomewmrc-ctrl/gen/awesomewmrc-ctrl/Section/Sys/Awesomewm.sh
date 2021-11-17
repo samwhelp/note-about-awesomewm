9cd09319fac1e8e9f663f9546f33a5c9276f039e
samwhelp <samwhelp@users.noreply.github.com>
2021-11-17 12:04:55 +0800

    fix

--- _demo/project/awesomewmrc-profile/awesomewmrc-ctrl/gen/awesomewmrc-ctrl/Section/Sys/Awesomewm.sh
+++ _demo/project/awesomewmrc-profile/awesomewmrc-ctrl/gen/awesomewmrc-ctrl/Section/Sys/Awesomewm.sh
@@ -5,10 +5,11 @@
 ##
 
 is_awesomewmrc_dir () {
-	local dir_path="$1"
-	local awesomewmrc_file_path="$dir_path/rc.lua"
 
-	if [ ! -f "$awesomewmrc_file_path" ]; then
+	local dir_path="$1"
+	local awesomewmrc_file_path="$dir_path/$THE_AWESOMEWMRC_FILE_NAME" ## ~/.config/awesomewmrc-profile/demo/awesomewm.conf
+
+	if [ ! -f "$awesomewmrc_file_path" ]; then ## check ~/.config/awesomewmrc-profile/demo/awesomewm.conf
 		return 1
 	fi
 
@@ -16,14 +17,16 @@ is_awesomewmrc_dir () {
 }
 
 is_not_awesomewmrc_dir () {
-	local dir_path="$1"
-	local awesomewmrc_file_path="$dir_path/rc.lua" ## ~/.awesomewmrc-profile/demo/rc.lua
 
-	if [ -f "$awesomewmrc_file_path" ]; then ## check ~/.awesomewmrc-profile/demo/rc.lua
+	local dir_path="$1"
+	local awesomewmrc_file_path="$dir_path/$THE_AWESOMEWMRC_FILE_NAME" ## ~/.config/awesomewmrc-profile/demo/awesomewm.conf
+
+	if [ -f "$awesomewmrc_file_path" ]; then ## check ~/.config/awesomewmrc-profile/demo/awesomewm.conf
 		return 1
 	fi
 
 	return 0
+
 }
 
 awesomewmrc_find_dir_path_by_name () {
@@ -34,57 +37,94 @@ awesomewmrc_find_dir_path_by_name () {
 
 awesomewmrc_check_awesomewmrc_dir_exists () {
 
-	local awesomewmrc_dir_name="awesome"
-	local awesomewmrc_dir_path="$HOME/.config/$awesomewmrc_dir_name"
+	local awesomewmrc_dir_name="$THE_AWESOMEWMRC_DIR_NAME"
+	local awesomewmrc_dir_path="$THE_AWESOMEWMRC_DIR_PATH"
 
 	if ! [ -a "$awesomewmrc_dir_path" ]; then ## file not exists
 		return 0
 	fi
 
 
-	if [ -h "$awesomewmrc_dir_path" ]; then ## ~/.config/awesome is symbolic link
+	if [ -h "$awesomewmrc_dir_path" ]; then ## ~/.config/awesomewm is symbolic link
 		return 0
 	fi
 
 
 
-	echo "Dir is exists:" "$awesomewmrc_dir_path"
-	echo
+	util_error_echo "## Dir is exists:" "$awesomewmrc_dir_path"
+	util_error_echo
 
-	echo "Try to backup:"
-	echo
+	util_error_echo "## Try to backup:"
+	util_error_echo
 
-	local now=$(date +%Y%m%d_%s)
-	local bak_dir_path="${HOME}/.backup/awesomewmrc.bak"
+	local now="$(date +%Y%m%d_%s)"
+	local bak_dir_path="${HOME}/.backup/${awesomewmrc_dir_name}.bak"
 	local bak_target_path="${bak_dir_path}/${awesomewmrc_dir_name}.bak.${now}"
 
-	echo "mkdir -p ${bak_dir_path}"
+	util_error_echo "mkdir -p ${bak_dir_path}"
 	mkdir -p "${bak_dir_path}"
 
 
-	echo "cp -a ${awesomewmrc_dir_path} ${bak_target_path}"
+	util_error_echo "cp -a ${awesomewmrc_dir_path} ${bak_target_path}"
 	cp -a "${awesomewmrc_dir_path}" "${bak_target_path}"
 
 	if [ "$?" != "0" ]; then
-		echo
-		echo 'Backup Failure!'
+		util_error_echo
+		util_error_echo '## Backup Failure!'
 		#exit 1
 		return 1
 	fi
 
-	echo
-
-
-
-
-
+	util_error_echo
 
 
 	return 0
 }
 
 
+awesomewmrc_profile_init_dir () {
 
+	if ! [ -d "$THE_AWESOMEWMRC_PROFILE_DIR_PATH" ]; then
+		##echo "$THE_AWESOMEWMRC_PROFILE_DIR_PATH"
+		mkdir -p "$THE_AWESOMEWMRC_PROFILE_DIR_PATH"
+	fi
+
+	return 0
+}
+
+
+awesomewmrc_repo_clone () {
+
+	## $ awesomewmrc-get demo
+
+	local name="$1"
+	local repo_url="$2"
+
+	if [ "none$name" == 'none' ]; then
+		name='default'
+	fi
+
+	util_error_echo "mkdir -p $THE_AWESOMEWMRC_PROFILE_DIR_PATH"
+	mkdir -p "$THE_AWESOMEWMRC_PROFILE_DIR_PATH"
+
+	util_error_echo "cd $THE_AWESOMEWMRC_PROFILE_DIR_PATH"
+	cd "$THE_AWESOMEWMRC_PROFILE_DIR_PATH" ## cd ~/.local/share/awesomewmrc-profile
+
+
+	if [ -d "$name" ]; then
+		util_error_echo
+		util_error_echo "## Is Exists:" "$THE_AWESOMEWMRC_PROFILE_DIR_PATH/$name"
+		return 0;
+	fi
+
+	util_error_echo "git clone --recursive $repo_url $name "
+	git clone --recursive "$repo_url" "$name"  ## git clone --recursive https://github.com/conformal/awesomewm.git demo
+
+	## $ man cd
+	util_error_echo "cd $OLDPWD"
+	cd "$OLDPWD"
+
+}
 
 ##
 ### Tail: Sys
